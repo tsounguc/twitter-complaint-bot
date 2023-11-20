518f48296f3c569ab063389eebcb190bfd974737
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-20 15:49:23 -0500

    Driver cannot find google sign in button

--- internet_speed_bot.py
+++ internet_speed_bot.py
@@ -1,7 +1,12 @@
+import os
 import time
 
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 class InternetSpeedTwitterBot:
@@ -24,5 +29,28 @@ class InternetSpeedTwitterBot:
         print(f"Upload speed: {self.up} Mbps")
 
     def tweet_at_provider(self):
-        pass
+        self.driver.get("https://twitter.com/")
 
+        time.sleep(10)
+        sign_in_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div['
+                                                            '1]/div[1]/div/div[3]/div[5]/a/div')
+        sign_in_button.click()
+
+        # time.sleep(10)
+        # sign_in_window = self.driver.window_handles[1]
+        # self.driver.switch_to.window(sign_in_window)
+
+        google_button = WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/div/div[2]/span[1]')))
+        google_button.click()
+
+        time.sleep(10)
+        sign_in_window = self.driver.window_handles[1]
+        self.driver.switch_to.window(sign_in_window)
+
+        email = os.environ.get("email", "Couldn't find email address")
+        username = os.environ.get("username", "Couldn't find username")
+        password = os.environ.get("password", "Couldn't find password")
+
+        email_input = self.driver.find_element(By.XPATH, '//*[@id="identifierId"]')
+        email_input.send_keys(email)
+        email_input.send_keys(Keys.ENTER)
