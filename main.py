3ea66ba1612f072767200f0fcbbc75294ecec7d7
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-20 13:13:29 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+CHROME_DRIVER_PATH= ''
+TWITTER_EMAIL= os.environ.get("email", "Couldn't find email address")
+TWITTER_PASSWORD = os.environ.get("password", "Couldn't find password")
+
+
